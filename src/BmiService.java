3cbb9f6c855c30cf8946c048a1c7a5f8bfacb62f
Anastasia <vesheneba2020@gmail.comgit config --global user.email vesheneba2020@gmail.com>
2024-06-10 19:39:29 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+
+public int calculate(int w, double h){
+    double rezult;
+    rezult=w/(h*h);
+    return (int)rezult;
+
+}
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+ BmiService index = new BmiService();
+int MyIndex = index.calculate(98,1.87);
+
+System.out.println(MyIndex);
+    }
+    }
